da3d7dab2062c72202eef5224094bc06da755fe3
Samiran Sarkar <pappu@gmail.com>
2026-01-20 02:00:26 +0530

    3 mutation and 2 query

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,20 +1,103 @@
 
 
 import { users } from "../dummyData/data.js";
+import User from "../models/user.model.js";
 const userResolver = {
-    Query:{
-        users: () =>{
-            return users;
-        },
-        user: (_, {userId})=>{
-            return users.find(user => user._id === userId);
-
-        }
-
-    },
     Mutation:{
+        signUp: async(_, {input}, context) => {
+            try {
+                const {username, name, password, gender} = input;
+                if(!username || !name || !password || !gender){
+                    throw new Error("All fields are required");
+                }
+                const existingUser = await User.findOne({username});
+                if(existingUser){
+                    throw new Error("Username already taken");
+                }
 
+                const salt = await bcrypt.genSalt(10);
+                const hashedPassword = await bcrypt.hash(password, salt);
+
+                const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+                const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+                const newUser = new User({
+                    username,
+                    name,
+                    password: hashedPassword,
+                    gender,
+                    profilePicture: gender === "male" ? boyProfilePic : girlProfilePic,
+                })
+                await newUser.save();
+                await context.login(newUser);
+                return newUser;
+                
+            } catch (error) {
+                console.error("Signup error:", error);
+                throw new Error("Error during sign up: " + error.message);
+                
+            }
+        },
+        login: async(_, {input}, context) => {
+            try {
+                const {username, password} = input;
+                await context.authenticate('graphql-local', {username, password});
+
+                await context.login(user);
+                return user;
+            } catch (error) {
+                console.error("Login error:", error);
+                throw new Error("error during login" + error.message);
+            }
+        },
+        logout: async(_,_, context) => {
+            try {
+                await context.logout();
+                req.session.destroy(
+                    (error) => {
+                        if(error){
+                            throw new Error("Error during logout" + error.message);
+                        }
+                    }
+                )
+                res.clearCookie('connect.sid');
+                return {
+                    success: true,
+                    message: "Logged out successfully",
+                }
+            } catch (error) {
+                console.error("Logout error:", error);
+                throw new Error("Error during logout" + error.message);
+                
+            }
+        }
+        
     },
+    Query:{
+        authUser: async(_, _, context) => {
+            try {
+                const user = context.getUser();
+                return user;
+                
+            } catch (error) {
+                console.error("AuthUser error:", error);
+                throw new Error("Error during authUser" + error.message);
+                
+            }
+
+        },
+        user: async(_, {userId}) => {
+            try {
+                const user = await User.findById(userId);
+                return user;
+            } catch (error) {
+                console.error("User error:", error);
+                throw new Error( error.message || "Error during user" );
+            }
+        }
+    },
+    // TODO => add the transactions field resolver
+
 };
 
 
